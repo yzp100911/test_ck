4a17a43e3810b56b7627354948f41b7ada6a73a2
yzp100911 <1178375453@qq.com>
2019-04-28 20:28:49 +0800

    Default Git_ck

---
+++ src/YZP.java
@@ -0,0 +1,21 @@
+public class YZP {
+
+    static YZP dz = new YZP();
+    public static   int a(int b, int c) {
+        var d = b + c;
+        return d;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(dz);
+        //
+    }
+
+}
+class yzp1 extends YZP{
+    public static void main(String[] args) {
+        System.out.println(dz.a(1,2));
+        //
+    }
+}
+//999999999999999
